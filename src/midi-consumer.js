b5a28314813a0bed0543a04ecee3ddf8f5ec7618
Josh Beck <j.beck18@me.com>
2019-09-16 13:02:07 -0400

    Added sound

--- src/midi-consumer.js
+++ src/midi-consumer.js
@@ -5,6 +5,22 @@ const SoundFont = require('soundfont-player');
 
 let midi = null;
 
+var ac = null;
+var piano = null;
+
+var notes = {};
+
+function createPianoSound() {
+    if(ac === null) {
+        ac = new AudioContext();
+
+        SoundFont.instrument(ac, 'acoustic_grand_piano').then(function(res) {
+            piano = res;
+        });
+    }
+}
+document.getElementById('start-sound').onclick = createPianoSound;
+
 const handleKeyPress = (key, data) => {
     const color = document.getElementById('pressed-color').value;
     if(color !== 'random') {
@@ -40,7 +56,7 @@ export function handleMIDIEvent(data) {
     const key = document.getElementById(keyValue + '');
 
     Broadcaster.handleEvent(key, data);
-    
+    console.log(data);
     switch(eventType) {
         // Key pressed
         case 144:
@@ -51,6 +67,21 @@ export function handleMIDIEvent(data) {
         case 128:
             handleKeyDepress(key, data);
             break;
+    }
+
+    if(piano !== null) {
+        if(eventType === 144) {
+            let key = data[1];
+            let volume = (data[2] / 127)
+            volume = parseFloat(Math.round(volume * 100) / 100);
+
+            let val = piano.play(key, ac.currentTime, { gain: volume });
+            notes[key] = val;
+        } else if(eventType === 128) {
+            let key = data[1];
+            notes[key].stop(ac.currentTime + 0.01);
+            notes[key] = null;
+        }
     }
 };
 
